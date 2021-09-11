8af8942055d6cf3cb67cf08d7eaaa85e5a8c52b0
rapidacc <30630140+rapidacc@users.noreply.github.com>
2021-09-11 17:44:49 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,80 +1,8 @@
-# LineageOS (crDroid)
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
-
-# HavocOS
+# dotos
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/havoc_santoni.mk
+    $(LOCAL_DIR)/dotos_santoni.mk
 
 COMMON_LUNCH_CHOICES += \
-    havoc_santoni-userdebug \
-    havoc_santoni-user \
-    havoc_santoni-eng
-
-# AOSiP
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosip_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosip_santoni-userdebug \
-    aosip_santoni-user \
-    aosip_santoni-eng
-
-# Bliss
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/bliss_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    bliss_santoni-userdebug \
-    bliss_santoni-user \
-    bliss_santoni-eng
-
-# Derpfest
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/derp_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    derp_santoni-userdebug \
-    derp_santoni-user \
-    derp_santoni-eng
-
-# Cherish
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/cherish_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    cherish_santoni-userdebug \
-    cherish_santoni-user \
-    cherish_santoni-eng
-
-# Pixel Experience
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosp_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosp_santoni-userdebug \
-    aosp_santoni-user \
-    aosp_santoni-eng
-
-# Corvus
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/corvus_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    corvus_santoni-userdebug \
-    corvus_santoni-user \
-    corvus_santoni-eng
-
-# RevengeOS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/revengeos_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    revengeos_santoni-userdebug \
-    revengeos_santoni-user \
-    revengeos_santoni-eng
+    dotos_santoni-userdebug \
+    dotos_santoni-user \
+    dotos_santoni-eng
